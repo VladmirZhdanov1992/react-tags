fd58f0a9ed6f724e7ebadf55074061b54c4e9022
Vladimir <vladimir@luna.kg>
2016-08-31 08:34:56 +0600

    fork from react-tags-autocomplete

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './example/main.js',
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'buble', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { 'NODE_ENV': '"production"' }
+    })
+  ],
+  output: {
+    filename: 'example/bundle.js'
+  },
+  // resolve: {
+  //   alias: {
+  //     'react': 'preact-compat',
+  //     'react-dom': 'preact-compat'
+  //   }
+  // }
+}
